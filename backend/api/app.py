1f791d347c0e470d1873e621ec9c08d815109fcb
Arnold <arnoldnderitu@gmail.com>
2024-07-04 19:25:43 +0300

    install and initialize alchemical Alchemical is a SQLAlchemy ORM wrapper that provides a simplified interface for SQLAlchemy

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,8 +1,13 @@
 from flask import Config, Flask
+from alchemical.flask import Alchemical
+
+db = Alchemical()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    db.init_app(app)
 
     return app
