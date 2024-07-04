ddf8e73f0ed7700b2d14f2e99b56f5fed6b7c404
Arnold <arnoldnderitu@gmail.com>
2024-07-04 22:42:19 +0300

    allow longer scrypt password hashes

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,6 +3,7 @@ import sqlalchemy as sa
 from api import db
 from sqlalchemy import orm as so
 from alchemical import Model
+from werkzeug.security import generate_password_hash
 
 
 class User(Model):
@@ -11,4 +12,16 @@ class User(Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
-    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+    @property
+    def has_password(self):
+        return self.password_hash is not None
+
+    @property
+    def password(self):
+        raise AttributeError("passwordis not a readable attribute")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
