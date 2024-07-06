c0143665932a6f5f1cc2251c15ffe8f544f6832c
Arnold <arnoldnderitu@gmail.com>
2024-07-06 15:38:27 +0300

    Add tab navigation with custom icons and styles

--- mobile-app/app/(tabs)/_layout.tsx
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -1,11 +1,91 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { Tabs } from 'expo-router';
+import { TabIconProps } from '../types/TabProps';
+import { View, Image, Text } from 'react-native';
+import icons from "../../constants/icons";
+
+const TabIcon: React.FC<TabIconProps> = ({ icon, color, name, focused }) => {
+    return (
+        <View className='flex items-center justify-center gap-2'>
+            <Image
+                source={icon}
+                resizeMode='contain'
+                tintColor={color}
+                className='h-6 w-6'
+            />
+            <Text
+                className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`}
+                style={{ color: color }}
+            >
+                {name}
+            </Text>
+        </View>
+    )
+}
 
 const TabsLayout = () => {
     return (
-        <View>
-            <Text>TabsLayout</Text>
-        </View>
+        <>
+            <Tabs
+                screenOptions={{
+                    tabBarActiveTintColor: '#FFA001',
+                    tabBarInactiveTintColor: '#CDCDE0',
+                    tabBarShowLabel: false,
+                    tabBarStyle: {
+                        backgroundColor: '#161622',
+                        borderTopWidth: 1,
+                        borderTopColor: '#232533',
+                        height: 84
+                    },
+                }}
+            >
+                <Tabs.Screen
+                    name="home"
+                    options={{
+                        title: "Home",
+                        headerShown: false,
+                        tabBarIcon: ({ color, focused }) => (
+                            <TabIcon
+                                icon={icons.home}
+                                color={color}
+                                name='Home'
+                                focused={focused}
+                            />
+                        )
+                    }}
+                />
+                <Tabs.Screen
+                    name="create"
+                    options={{
+                        title: "Create",
+                        headerShown: false,
+                        tabBarIcon: ({ color, focused }) => (
+                            <TabIcon
+                                icon={icons.plus}
+                                color={color}
+                                name="Create"
+                                focused={focused}
+                            />
+                        ),
+                    }}
+                />
+                <Tabs.Screen
+                    name="profile"
+                    options={{
+                        title: "Profile",
+                        headerShown: false,
+                        tabBarIcon: ({ color, focused }) => (
+                            <TabIcon
+                                icon={icons.profile}
+                                color={color}
+                                name="Profile"
+                                focused={focused}
+                            />
+                        ),
+                    }}
+                />
+            </Tabs>
+        </>
     )
 }
 
