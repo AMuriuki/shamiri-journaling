0ab62db5aa24728d7e15a480bf92437faf0eca5f
Arnold <arnoldnderitu@gmail.com>
2024-07-08 12:35:18 +0300

    increase access token lifespan

--- backend/config.py
+++ backend/config.py
@@ -15,6 +15,6 @@ class Config:
     ALCHEMICAL_DATABASE_URL = os.environ.get("DATABASE_URL")
     ALCHEMICAL_ENGINE_OPTIONS = {"echo": as_bool(os.environ.get("SQL_ECHO", "false"))}
     USE_CORS = as_bool(os.environ.get("USE_CORS") or "yes")
-    ACCESS_TOKEN_MINUTES = int(os.environ.get("ACCESS_TOKEN_MINUTES") or "15")
+    ACCESS_TOKEN_MINUTES = int(os.environ.get("ACCESS_TOKEN_MINUTES") or "60")
     REFRESH_TOKEN_DAYS = int(os.environ.get("REFRESH_TOKEN_DAYS") or "7")
     SECRET_KEY = os.environ.get('SECRET_KEY', 'top-secret!')
